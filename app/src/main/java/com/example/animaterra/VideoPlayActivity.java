371a0caf7f7d8ddbfbc8cc179975510c098f2384
Jipa Teodora <teo.tem06@yahoo.com>
2020-03-09 22:40:28 +0200

    TJ: added buttons for record a video and play a video

---
+++ app/src/main/java/com/example/animaterra/VideoPlayActivity.java
@@ -0,0 +1,33 @@
+package com.example.animaterra;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.VideoView;
+
+public class VideoPlayActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_play);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mVideoView = findViewById(R.id.animalsVideoView);
+
+        Uri videoUri = Uri.parse(getIntent().getExtras().getString("video"));
+        mVideoView.setVideoURI(videoUri);
+        mVideoView.start();
+    }
+
+}
